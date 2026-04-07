b9ee8ead24698145263ad03ab691d23f2f00eaf1
wantbycre <wantbycre@gmail.com>
2026-04-07 12:05:43 +0900

    여백수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="mx-auto max-w-2xl space-y-6">
+        <div className="mx-auto max-w-2xl space-y-6 px-4">
           <AppNav />
           {children}
         </div>
